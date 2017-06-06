fec0851c905a68173002c63eb50d21c3fa08f3ce
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-06 15:40:44 +0000

    Add insertionSort

---
+++ sort.js
@@ -0,0 +1,27 @@
+function insertionSort(arr) {
+    for ()
+}
+
+
+[3, 1, 2]
+
+
+
+let arr = [3, 4, 6, 1, 9, 2];
+console.log(insertionSort(arr));
+
+
+
+
+for (let i = 0; i < arr.length - 1; i++) {
+        let lowestNumIndex = i;
+        
+        for (let j = i + 1; j < arr.length; j++) {
+            //find the lowest number
+            if (arr[j] < arr[lowestNumIndex]) {
+                lowestNumIndex = arr[j];
+            }
+            //
+            
+        }
+    }
